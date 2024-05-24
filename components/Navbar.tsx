f2df58c0aabce80c011054595a2e982fe42b276f
sunandasamanta <samantasunanda724@gmail.com>
2024-05-24 18:45:51 +0530

    init by sunandasamanta

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-wrap gap-4 justify-around content-center h-24 bg-[#21222A] shadow-2xl ">
+        <h2 className="text-[#61DAFB] font-medium text-xl">ReactFacts</h2>
+        <p className="text-[#DEEBF8]">React Course - Project 1</p>
+    </nav>
+  )
+}
