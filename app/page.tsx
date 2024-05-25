698dcb37a4a0f88fb31cea8b0f6cd3c5846538c1
sunandasamanta <samantasunanda724@gmail.com>
2024-05-25 09:28:09 +0530

    chore: Update page title and description in layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <main className="bg-[#282D35] h-screen">
       <Navbar />
-      <div className="text-white w-max m-28">
-        <h1 className="font-bold text-5xl mb-10">Fun facts about React</h1>
-        <div className="mx-16">
-          <p className="">Was first released in 2013</p>
-          <p className="">Was originally created by Jordan Walke</p>
-          <p className="">Has well over 100k stars on GitHub</p>
-          <p className="">Is maintained by the Meta</p>
-          <p className="">Powers thousands of enterprise apps</p>
-        </div>
+      <div className="text-white w-max m-20">
+        <h1 className="font-bold text-5xl mb-6">Fun facts about React</h1>
+        <ul className="mx-16 list-disc disc-blue">
+          <li className="">Was first released in 2013</li>
+          <li className="">Was originally created by Jordan Walke</li>
+          <li className="">Has well over 100k stars on GitHub</li>
+          <li className="">Is maintained by the Meta</li>
+          <li className="">Powers thousands of enterprise apps</li>
+        </ul>
       </div>
     </main>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+
+
+//importing modules
+import Image from "next/image"
+
+
+// importing 
+import reactLogo from "@/public/react-logo.png"
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-wrap gap-4 justify-around content-center h-24 bg-[#21222A] shadow-2xl ">
-        <h2 className="text-[#61DAFB] font-medium text-xl">ReactFacts</h2>
+    <nav className="flex flex-wrap gap-4 justify-between px-8 content-center h-20 bg-[#21222A] shadow-2xl">
+        <div className="flex gap-1">
+          <Image src={reactLogo} className="w-7" alt="" />
+          <h2 className="text-[#61DAFB] font-medium text-xl">ReactFacts</h2>
+        </div>
         <p className="text-[#DEEBF8]">React Course - Project 1</p>
     </nav>
   )
